0b2f786cedf0fec080d2e64fb302b0dd9f190aa0
jyoti ade <jyotiade287@gmail.com>
2024-06-26 22:32:33 +0530

    urls inside app

---
+++ project1/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app import views 
+
+
+urlpatterns = [
+    path('app/',views.home),
+    
+]
---
+++ project1/app2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app2 import views 
+
+
+urlpatterns = [
+    path('app2/',views.home),
+    
+]
