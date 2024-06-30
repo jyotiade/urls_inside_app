74fc307bf20f7daffe118817ffbe3367c0f6d896
jyoti ade <jyotiade287@gmail.com>
2024-06-30 22:43:29 +0530

    new url

--- project1/app/urls.py
+++ project1/app/urls.py
@@ -4,6 +4,9 @@ from app import views
 
 urlpatterns = [
     path('app/',views.home),
+    # path('register/',views.register),
     path('register/',views.register),
-    
+    path('new/',views.new,name='new'),
+    path('registerdata/',views.registerdata,name='new'),
+   
 ]
--- project1/app/views.py
+++ project1/app/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1>welcome t0 home 1</h1>")
+# def home(request):
+#     return HttpResponse("<h1>welcome t0 home 1</h1>")
 
 def register(request):
     return render(request,'register.html')
-    
+
+def home(request):
+    return redirect("https://www.jiocinema.com/")
+
+def new(request):
+    return render(request,'new.html')
+
+def registerdata(request):
+    print(request.method)
+    print(request.POST)
