115e742f2b11b00eb3ad1570e0260feb9ae7015c
jyoti ade <jyotiade287@gmail.com>
2024-07-02 22:04:53 +0530

    new

--- project1/app/models.py
+++ project1/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#table related work
+
+class Student(models.Model):
+    Name=models.CharField(max_length=50)
+    Email=models.EmailField()
+    Contact=models.IntegerField()
--- project1/app/views.py
+++ project1/app/views.py
@@ -18,3 +18,8 @@ def new(request):
 def registerdata(request):
     print(request.method)
     print(request.POST)
+    
+    name=request.POST.get('name')
+    email=request.POST.get('email')
+    contact=request.POST.get('contact')
+    print(name,email,contact)
