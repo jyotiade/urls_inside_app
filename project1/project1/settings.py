92522d7074c4f4e1355bcdc5d972de2236dc5277
jyoti ade <jyotiade287@gmail.com>
2024-07-05 23:32:25 +0530

    template

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
-
+TEMPLATES_DIRS=os.path.join(BASE_DIR,'jyoti')
+print(TEMPLATES_DIRS)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
